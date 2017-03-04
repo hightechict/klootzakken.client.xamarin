7b3f11e0f075464bc7b3d8cb38ecbdd122d982ed
dmoka <daniel.moka@hightechict.nl>
2017-03-04 23:27:59 +0100

    Refactoring the WebApiGetService

--- Klootzakken.Client/Services/WebApiGetService.cs
+++ Klootzakken.Client/Services/WebApiGetService.cs
@@ -10,41 +10,41 @@ namespace KlootzakkenClient
     public static class WebApiGetService
     {
         //TODO: refactor it like in PostService
-        public static async Task<List<LobbyView>> GetMyGames()
+        public static async Task<List<LobbyView>> GetMyGamesAsync()
         {
-            return await GetFromWebApi<LobbyView>("myGames");
+            return await GetFromWebApiAsync<LobbyView>("myGames");
         }
 
-        public static async Task<List<LobbyView>> GetMyLobbies()
+        public static async Task<List<LobbyView>> GetMyLobbiesAsync()
         {
-            return await GetFromWebApi<LobbyView>("myLobbies");
+            return await GetFromWebApiAsync<LobbyView>("myLobbies");
         }
 
-        public static async Task<List<LobbyView>> GetLobbies()
+        public static async Task<List<LobbyView>> GetLobbiesAsync()
         {
-            return await GetFromWebApi<LobbyView>("lobbies");
+            return await GetFromWebApiAsync<LobbyView>("lobbies");
         }
 
-        public static async Task<GameView> GetGameState(string gameId)
+        public static async Task<GameView> GetGameStateAsync(string gameId)
         {
-            return await GetStateFromWebApi<GameView>("game", gameId);
+            return await GetStateFromWebApiAsync<GameView>("game", gameId);
         }
 
-        public static async Task<LobbyView> GetLobbyState(string lobbyId)
+        public static async Task<LobbyView> GetLobbyStateAsync(string lobbyId)
         {
-            return await GetStateFromWebApi<LobbyView>("lobby", lobbyId);
+            return await GetStateFromWebApiAsync<LobbyView>("lobby", lobbyId);
         }
 
+        private const string _baseUrl = "http://10.0.2.2:5000/";
+        private const string _accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIwNGIxYmU1NS04MmE5LTRhMTItODMzZC05ZjNlNzZiMTBjMzQiLCJ1bmlxdWVfbmFtZSI6ImRhbmllbC5tb2thQGhpZ2h0ZWNoaWN0Lm5sIiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiI2MWZkZDExNC0wMzRiLTQ3ZDYtYTk1ZS0wZDQ1YzBjZmYwM2YiLCJuYmYiOjE0ODg2NTkxNDEsImV4cCI6MTQ5MTMzNzU0MSwiaWF0IjoxNDg4NjU5MTQxLCJpc3MiOiJLbG9vdHpha2tlbiBTZXJ2ZXIiLCJhdWQiOlsiQXBpVXNlcnMiLCJBcGlVc2VycyJdfQ.V-D6HQSLYVjNOwakMXlsBAbbExpzGhA_kexQSwGHYZE";
 
-        private const string _accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI3YmUyMjI1NS0xMTFkLTQyNjUtYjkzNi0zY2I3NDQ2NWVmZGQiLCJ1bmlxdWVfbmFtZSI6ImRhbmllbC5tb2thQGhpZ2h0ZWNoaWN0Lm5sIiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiI4YTIyMTcwMC1kYjYzLTRiOWYtYWNiOC1mZTJjMDFiOWZjZmMiLCJuYmYiOjE0ODgzMDQ4MTAsImV4cCI6MTQ4ODM5MTIxMCwiaWF0IjoxNDg4MzA0ODEwLCJpc3MiOiJEaXZ2ZXJlbmNlLmNvbSBLbG9vdHpha2tlbiIsImF1ZCI6IkRlbW9BdWRpZW5jZSJ9.ctjrQuq_3XItJIWs99jCV4f3uJWX11_7xlzIHtn89KE";
-
-        private static async Task<List<T>> GetFromWebApi<T>(string urlEnding)
+        private static async Task<List<T>> GetFromWebApiAsync<T>(string urlEnding)
         {
             var views = new List<T>();
 
             using (var client = new HttpClient())
             {
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://www.glueware.nl/Klootzakken/kzapi/" + urlEnding);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _baseUrl + urlEnding);
                 request.Headers.Authorization = new AuthenticationHeaderValue("bearer", _accessToken);
 
                 var response = await client.SendAsync(request);
@@ -56,13 +56,13 @@ namespace KlootzakkenClient
             return views;
         }
 
-        private static async Task<T> GetStateFromWebApi<T>(string urlEnding, string id)
+        private static async Task<T> GetStateFromWebApiAsync<T>(string urlEnding, string id)
         {
             T state;
 
             using (var client = new HttpClient())
             {
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://www.glueware.nl/Klootzakken/kzapi/" + urlEnding + "/" + id);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _baseUrl + urlEnding + "/" + id);
                 request.Headers.Authorization = new AuthenticationHeaderValue("bearer", _accessToken);
 
                 var response = await client.SendAsync(request);
@@ -74,5 +74,6 @@ namespace KlootzakkenClient
             return state;
         }
 
-    }
+
+}
 }
