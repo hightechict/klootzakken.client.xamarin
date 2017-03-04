14052966e15b06cb18cd78e5c4a4da14e703a209
dmoka <daniel.moka@hightechict.nl>
2017-03-04 19:59:54 +0100

    config changed in test project

--- Klootzakken.Client.Test/Program.cs
+++ Klootzakken.Client.Test/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +11,13 @@ namespace ConsoleApp1
 {
     class Program
     {
-        //This is test main app for testing codes from the klootzakken xamarin app
+        private const string _mainUrl = "http://localhost:5000/";
+        private const string _urlToCreatePin = _mainUrl + "pin/create/";
 
+        //This is test main app for testing codes from the klootzakken xamarin app
         static void Main(string[] args)
         {
-
-
+            Console.ReadKey();
         }
     }
 }
