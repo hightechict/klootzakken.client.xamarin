423a65167df6e44f6c10d1f8cb2d5bc115c45dce
dmoka <daniel.moka@hightechict.nl>
2017-03-14 20:01:31 +0100

    added test for AuthenticationController

---
+++ Klootzakken.Client.Test/App/Authentication/AuthenticationControllerTest.cs
@@ -0,0 +1,108 @@
+﻿using Klootzakken.Client.App.Authentication;
+using Klootzakken.Client.Domain;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using System.Web;
+using System;
+using System.Threading.Tasks;
+
+namespace Klootzakken.Client.Test.App.Authentication
+{
+    public class AuthenticationControllerTest
+    {
+        private const string _pinCode = "9129";
+        private const string _expectedTempAuthToken = "temporaryAuthToken123456789";
+
+        private readonly PinAuthenticationException exepctedPinAuthenticationException = new PinAuthenticationException("404");
+
+        [Fact]
+        public void authenticationController_getPinCode_returnsPinCode()
+        {
+            //ARRANGE
+            var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            mockedAuthenticationService.Setup(auth => auth.GetPinAsync()).ReturnsAsync(_pinCode);
+
+            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+
+            //ACT
+            var pinCode = sut.GetPinCodeAsync().Result;
+
+            //ASSERT
+            pinCode.Should().BeAssignableTo<string>().And
+                .BeSameAs(_pinCode);
+        }
+        //TODO: write test for the situatin when no pin code can be acquired and have rensponse error(code)
+
+        [Fact]
+        public void getTemporaryAuthTokenThrowsPinAuthenticationException_pollinForTemporaryAuthToken_throwsPinAuthException()
+        {
+            //ARRANGE
+            var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            mockedAuthenticationService.Setup(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>())).Throws(exepctedPinAuthenticationException);
+
+            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+
+            //ACT
+            Func<Task> pollForTemporaryAuthTokenFunction = async () => await sut.pollingForTemporaryAuthToken(_pinCode, 1, 0);
+
+            //ASSERT
+            pollForTemporaryAuthTokenFunction.ShouldThrow<PinAuthenticationException>();
+        }
+
+        [Fact]
+        public void getTemporaryAuthTokenReturnsTheToken_pollingForTemporaryAuthToken_returnTemporaryAuthToken()
+        {
+            //ARRANGE
+            var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            mockedAuthenticationService.Setup(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>())).ReturnsAsync(_expectedTempAuthToken);
+
+            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+
+            //ACT
+            var tempAuthToken = sut.pollingForTemporaryAuthToken(_pinCode, 1, 0).Result;
+
+            //ASSERT
+            tempAuthToken.Should().BeAssignableTo<string>().And
+                .BeSameAs(_expectedTempAuthToken);
+        }
+
+        [Fact]
+        public void getTemporaryAuthTokenFirstThrowServiceExceptionThenReturnsTheTempToken_pollingForTempAuthToken_returnTempAuthToken()
+        {
+            //ARRANGE
+            var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            mockedAuthenticationService.SetupSequence(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>()))
+                .Throws(exepctedPinAuthenticationException)
+                .ReturnsAsync(_expectedTempAuthToken);
+
+            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+
+            //ACT
+            var tempAuthToken = sut.pollingForTemporaryAuthToken(_pinCode, 2, 0).Result;
+
+            //ASSERT
+            tempAuthToken.Should().BeAssignableTo<string>().And
+                  .BeSameAs(_expectedTempAuthToken);
+        }
+
+        [Fact]
+        public void getTemporaryAuthTokenThrowsExceptionsTillReachMaxNumberOfAttempts_pollingForTempAuthToken_returnsPinAuthenticationException()
+        {
+            //ARRANGE
+            var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            mockedAuthenticationService.SetupSequence(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>()))
+                .Throws(exepctedPinAuthenticationException)
+                .Throws(exepctedPinAuthenticationException)
+                .Throws(exepctedPinAuthenticationException);
+
+            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+
+            //ACT
+            Func<Task> pollForTemporaryAuthTokenFunction = async () => await sut.pollingForTemporaryAuthToken(_pinCode, 3, 0);
+
+            //ASSERT
+            pollForTemporaryAuthTokenFunction.ShouldThrow<PinAuthenticationException>();
+        }
+    }
+}
