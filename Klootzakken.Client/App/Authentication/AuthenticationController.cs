41ca3e2895cc35445acd4c512c670eb75743f503
dmoka <daniel.moka@hightechict.nl>
2017-03-20 19:52:07 +0100

    changed authentication controller for saving the bearer token to shared preference

--- Klootzakken.Client/App/Authentication/AuthenticationController.cs
+++ Klootzakken.Client/App/Authentication/AuthenticationController.cs
@@ -2,21 +2,44 @@ using System;
 using Klootzakken.Client.Domain;
 using System.Threading.Tasks;
 using System.Threading;
+using Android.App;
+using Android.Content;
+using Klootzakken.Client.Utils.Interfaces;
 
 namespace Klootzakken.Client.App.Authentication
 {
     public class AuthenticationController
     {
-        private IAuthenticationService _authenticationService;
+        private const string _brearerTokenKeyName = "bearer_token";
 
-        public AuthenticationController(IAuthenticationService authenticationService)
+        private IAuthenticationService _authenticationService;
+        private ISharedPreferenceHandler _preferenceHandler;
+
+        public AuthenticationController(IAuthenticationService authenticationService, ISharedPreferenceHandler preferenceHandler)
         {
             _authenticationService = authenticationService;
+            _preferenceHandler = preferenceHandler;
         }
 
         public async Task<string> GetPinCodeAsync()
         {
             return await _authenticationService.GetPinAsync();
+        }
+
+        public async void SaveBearerAuthTokenAsync(string pinCode)
+        {
+            if (_preferenceHandler.GetPreference(_brearerTokenKeyName) is null)
+            {
+                string bearerToken = await GetBrearerTokenAsync(pinCode);
+                _preferenceHandler.SavePreference("bearer_token", bearerToken);
+            }
+        }
+
+        private async Task<string> GetBrearerTokenAsync(string pinCode)
+        {
+            var tempToken = await pollingForTemporaryAuthToken(pinCode, 5, 5000);
+            var bearerToken = await _authenticationService.GetBearerTokenAsync(tempToken);
+            return bearerToken;
         }
 
         public async Task<string> pollingForTemporaryAuthToken(string pinCode, int totalNumberOfAttempts, int delayBetweenEachPollingInMilisec) //TODO: get vars from options or config
@@ -28,29 +51,15 @@ namespace Klootzakken.Client.App.Authentication
                 {
                     return await _authenticationService.GetTemporaryAuthTokenAsync(pinCode);
                 }
-                catch(PinAuthenticationException ex)
+                catch (PinAuthenticationException ex)
                 {
                     numberOfAttempts++;
                     if (numberOfAttempts >= totalNumberOfAttempts)
                         throw; //TODO: do we want to return?
                     Thread.Sleep(delayBetweenEachPollingInMilisec);
-                    Console.WriteLine(ex); //TODO: write error to an error blok
                 }
             }
         }
 
-        public async Task<string> GetBearerAuthToken(string tempAuthToken)
-        {
-            try
-            {
-            return await _authenticationService.GetBearerTokenAsync(tempAuthToken);
-
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                throw;
-            }
-        }
     }
 }
