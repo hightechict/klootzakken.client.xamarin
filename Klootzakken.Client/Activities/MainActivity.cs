6d0cda9e420332409537e7b6c607f9bce080d9f8
dmoka <daniel.moka@hightechict.nl>
2017-03-20 19:53:25 +0100

    set up the services in the main activity in IoC container

--- Klootzakken.Client/Activities/MainActivity.cs
+++ Klootzakken.Client/Activities/MainActivity.cs
@@ -2,14 +2,45 @@
 using Android.Widget;
 using Android.OS;
 using Klootzakken.Client.Activities;
+using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight.Views;
+using GalaSoft.MvvmLight.Ioc;
+using Klootzakken.Client.Domain;
+using System;
+using Klootzakken.Client.App.Authentication;
+using Klootzakken.Client.Utils;
 
 namespace Klootzakken.Client
 {
     [Activity(Label = "KlootzakkenClient", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private static bool _iocContainerInitialized;
+        public const string Page2Key = "TestActivity";
+
         protected override void OnCreate(Bundle bundle)
         {
+
+            if (!_iocContainerInitialized)
+            {
+                _iocContainerInitialized = true;
+                ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+                var nav = new NavigationService();
+                nav.Configure(Page2Key, typeof(TestActivity));
+                SimpleIoc.Default.Register<INavigationService>(() => nav);
+
+                var dialog = new DialogService();
+                SimpleIoc.Default.Register<IDialogService>(() => dialog);
+            }
+
+            var authenticationOptions = new AuthenticationOptions() { BaseUri = new Uri("http://www.glueware.nl/klootzakken/kz/") };
+            var authenticationService = new AuthenticationService(authenticationOptions);
+            var authenticationController = new AuthenticationController(authenticationService, new SharedPreferenceHandler());
+            SimpleIoc.Default.Register<AuthenticationController>(() => authenticationController);
+
+
+
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
@@ -17,8 +48,9 @@ namespace Klootzakken.Client
 
             loginButton.Click += delegate
             {
-                StartActivity(typeof(TestActivityWebApi));
+                StartActivity(typeof(AuthorizationActivity));
             };
+            
         }
     }
 }
