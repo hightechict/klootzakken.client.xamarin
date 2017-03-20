824947421e24d82b306f322bd50862e777b2dcd6
dmoka <daniel.moka@hightechict.nl>
2017-03-20 19:52:55 +0100

    added authentication activity for pin auth

---
+++ Klootzakken.Client/Activities/AuthenticationActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Klootzakken.Client.Activities;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using Klootzakken.Client.App.Authentication;
+using Android.Content;
+
+namespace Klootzakken.Client
+{
+    [Activity(Label = "KlootzakkenClient", MainLauncher = false, Icon = "@drawable/icon")]
+    public class AuthorizationActivity : ActivityBase
+    {
+        public const string maniMenuPageKey = "MainMenuActivity";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.AuthenticationView);
+
+            var dialogService = ServiceLocator.Current.GetInstance<IDialogService>();
+            var nav = ServiceLocator.Current.GetInstance<INavigationService>();
+
+            Button generatePinButton = FindViewById<Button>(Resource.Id.generatePin);
+
+            generatePinButton.Click += async delegate
+            {
+                var authenticationController  = ServiceLocator.Current.GetInstance<AuthenticationController>();
+                var pinCode = await authenticationController.GetPinCodeAsync();
+
+                await dialogService.ShowMessage(
+                   pinCode,
+                    "Pair the following pincode",
+                    "Log in",
+                    "Cancel", 
+                    new Action<bool>((isConfirmed) => authenticationController.SaveBearerAuthTokenAsync(pinCode))
+                    );
+            };
+
+            //nav.NavigateTo(maniMenuPageKey); //TODO: navigate to
+        }
+    }
+}
