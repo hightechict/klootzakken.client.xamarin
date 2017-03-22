1973fb6b04ef5d683472b1a4147df1625a24a4fe
dmoka <daniel.moka@hightechict.nl>
2017-03-22 21:37:09 +0100

    added test with moch for authentication controller

--- Klootzakken.Client.Test/App/Authentication/AuthenticationControllerTest.cs
+++ Klootzakken.Client.Test/App/Authentication/AuthenticationControllerTest.cs
@@ -6,121 +6,78 @@ using FluentAssertions;
 using System.Web;
 using System;
 using System.Threading.Tasks;
+using Klootzakken.Client.Utils;
+using Klootzakken.Client.Utils.Interfaces;
+using Klootzakken.Client.App.Interfaces;
 
 namespace Klootzakken.Client.Test.App.Authentication
 {
     public class AuthenticationControllerTest
     {
         private const string _pinCode = "9129";
-        private const string _expectedAuthToken = "temporaryAuthToken123456789";
-
-        private readonly PinAuthenticationException exepctedPinAuthenticationException = new PinAuthenticationException("404");
+        private const string _expectedTempAuthToken = "temporaryAuthToken123456789";
+        private const string _bearerToken = "bearerToken123456789";
+        private const string _brearerTokenKeyName = "bearer_token";
 
         [Fact]
         public void authenticationController_getPinCode_returnsPinCode()
         {
-            //ARRANGE
+            //Arrange
             var mockedAuthenticationService = new Mock<IAuthenticationService>();
+            var mockedSharedPreference = new Mock<ISharedPreferenceHandler>();
+            var mockedTempAuthTokenPoller = new Mock<ITempAuthTokenPoller>();
+
             mockedAuthenticationService.Setup(auth => auth.GetPinAsync()).ReturnsAsync(_pinCode);
 
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+            var sut = new AuthenticationController(mockedAuthenticationService.Object, mockedTempAuthTokenPoller.Object, mockedSharedPreference.Object);
 
-            //ACT
+            //Act
             var pinCode = sut.GetPinCodeAsync().Result;
 
-            //ASSERT
+            //Assert
             pinCode.Should().BeAssignableTo<string>().And
                 .BeSameAs(_pinCode);
         }
         //TODO: write test for the situatin when no pin code can be acquired and have rensponse error(code)
 
         [Fact]
-        public void getTemporaryAuthTokenThrowsPinAuthenticationException_pollinForTemporaryAuthToken_throwsPinAuthException()
+        public void brearerTokenNotSavedAsSharedPreference_saveBearerToken_tokenIsSaved()
         {
-            //ARRANGE
+            //Arrange
+            var mockedSharedPreference = new Mock<ISharedPreferenceHandler>();
             var mockedAuthenticationService = new Mock<IAuthenticationService>();
-            mockedAuthenticationService.Setup(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>())).Throws(exepctedPinAuthenticationException);
+            var mockedTempAuthTokenPoller = new Mock<ITempAuthTokenPoller>();
 
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+            mockedSharedPreference.Setup(pref => pref.GetPreference(_brearerTokenKeyName)).Returns<string>(null);
+            mockedTempAuthTokenPoller.Setup(auth => auth.poll(_pinCode, It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(_expectedTempAuthToken);
+            mockedAuthenticationService.Setup(auth => auth.GetBearerTokenAsync(It.IsAny<string>())).ReturnsAsync(_bearerToken);
 
-            //ACT
-            Func<Task> pollForTemporaryAuthTokenFunction = async () => await sut.pollingForTemporaryAuthToken(_pinCode, 1, 0);
+            var sut = new AuthenticationController(mockedAuthenticationService.Object, mockedTempAuthTokenPoller.Object, mockedSharedPreference.Object);
 
-            //ASSERT
-            pollForTemporaryAuthTokenFunction.ShouldThrow<PinAuthenticationException>();
+            //Act
+            sut.SaveBearerAuthTokenAsync(It.IsAny<string>());
+
+            //Assert
+            mockedSharedPreference.Verify(mock => mock.SavePreference("bearer_token", _bearerToken), Times.Once());
         }
 
         [Fact]
-        public void getTemporaryAuthTokenReturnsTheToken_pollingForTemporaryAuthToken_returnTemporaryAuthToken()
+        public void brearerTokenIsAlreadySavedAsSharedPreference_saveBearerToken_tokenNotSavedAgain()
         {
-            //ARRANGE
+            //Arrange
+            var mockedSharedPreference = new Mock<ISharedPreferenceHandler>();
             var mockedAuthenticationService = new Mock<IAuthenticationService>();
-            mockedAuthenticationService.Setup(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>())).ReturnsAsync(_expectedAuthToken);
+            var mockedTempAuthTokenPoller = new Mock<ITempAuthTokenPoller>();
 
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
+            mockedSharedPreference.Setup(pref => pref.GetPreference(_brearerTokenKeyName)).Returns(_bearerToken);
 
-            //ACT
-            var tempAuthToken = sut.pollingForTemporaryAuthToken(_pinCode, 1, 0).Result;
+            var sut = new AuthenticationController(mockedAuthenticationService.Object, mockedTempAuthTokenPoller.Object, mockedSharedPreference.Object);
 
-            //ASSERT
-            tempAuthToken.Should().BeAssignableTo<string>().And
-                .BeSameAs(_expectedAuthToken);
-        }
+            //Act
+            sut.SaveBearerAuthTokenAsync(It.IsAny<string>());
 
-        [Fact]
-        public void getTemporaryAuthTokenFirstThrowServiceExceptionThenReturnsTheTempToken_pollingForTempAuthToken_returnTempAuthToken()
-        {
-            //ARRANGE
-            var mockedAuthenticationService = new Mock<IAuthenticationService>();
-            mockedAuthenticationService.SetupSequence(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>()))
-                .Throws(exepctedPinAuthenticationException)
-                .ReturnsAsync(_expectedAuthToken);
-
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
-
-            //ACT
-            var tempAuthToken = sut.pollingForTemporaryAuthToken(_pinCode, 2, 0).Result;
-
-            //ASSERT
-            tempAuthToken.Should().BeAssignableTo<string>().And
-                  .BeSameAs(_expectedAuthToken);
-        }
-
-        [Fact]
-        public void getTemporaryAuthTokenThrowsExceptionsTillReachMaxNumberOfAttempts_pollingForTempAuthToken_returnsPinAuthenticationException()
-        {
-            //ARRANGE
-            var mockedAuthenticationService = new Mock<IAuthenticationService>();
-            mockedAuthenticationService.SetupSequence(auth => auth.GetTemporaryAuthTokenAsync(It.IsAny<string>()))
-                .Throws(exepctedPinAuthenticationException)
-                .Throws(exepctedPinAuthenticationException)
-                .Throws(exepctedPinAuthenticationException);
-
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
-
-            //ACT
-            Func<Task> pollForTemporaryAuthTokenFunction = async () => await sut.pollingForTemporaryAuthToken(_pinCode, 3, 0);
-
-            //ASSERT
-            pollForTemporaryAuthTokenFunction.ShouldThrow<PinAuthenticationException>();
-            //TODO: return another exception
-        }
-
-        [Fact]
-        public void authenticationController_getBearerAuthToken_returnsBearerAuthToken()
-        {
-            //ARRANGE
-            var mockedAuthenticationService = new Mock<IAuthenticationService>();
-            mockedAuthenticationService.SetupSequence(auth => auth.GetBearerTokenAsync(It.IsAny<string>())).ReturnsAsync(_expectedAuthToken);
-
-            var sut = new AuthenticationController(mockedAuthenticationService.Object);
-
-            //ACT
-            var bearerToken = sut.GetBearerAuthToken(It.IsAny<string>()).Result;
-
-            //ASSERT
-            bearerToken.Should().BeAssignableTo<string>().And
-                .BeSameAs(_expectedAuthToken);
+            //Assert
+            mockedSharedPreference.Verify(mock => mock.SavePreference("bearer_token", _bearerToken), Times.Never);
         }
     }
 }
