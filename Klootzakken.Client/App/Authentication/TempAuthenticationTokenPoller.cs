2189fee2df74d496ce82f16ef8d321634bbff4c7
dmoka <daniel.moka@hightechict.nl>
2017-03-22 20:06:42 +0100

    added extracted temp auth token poller

---
+++ Klootzakken.Client/App/Authentication/TempAuthenticationTokenPoller.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Klootzakken.Client.Domain;
+using System.Threading;
+using System.Threading.Tasks;
+using Klootzakken.Client.App.Interfaces;
+
+namespace Klootzakken.Client.App.Authentication
+{
+    public class TempAuthenticationTokenPoller
+    {
+        private IAuthenticationService _authenticationService;
+
+        public TempAuthenticationTokenPoller(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        public async Task<string> poll(string pinCode, int totalNumberOfAttempts, int delayBetweenEachPollingInMilisec) //TODO: get vars from options or config
+        {
+            var numberOfAttempts = 0;
+            while (true)
+            {
+                try
+                {
+                    return await _authenticationService.GetTemporaryAuthTokenAsync(pinCode);
+                }
+                catch (PinAuthenticationException ex)
+                {
+                    numberOfAttempts++;
+                    if (numberOfAttempts >= totalNumberOfAttempts)
+                        throw; //TODO: do we want to return?
+                    Thread.Sleep(delayBetweenEachPollingInMilisec);
+                }
+            }
+        }
+
+    }
+}
