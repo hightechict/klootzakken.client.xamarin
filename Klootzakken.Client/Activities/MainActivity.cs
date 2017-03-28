2982fb4e1789fdbbf2aeed0094a0544fbf9a34c9
Daniel Moka <daniel.moka@hightechict.nl>
2017-03-28 20:57:12 +0200

    changed main activity with navigation keys and service

--- Klootzakken.Client/Activities/MainActivity.cs
+++ Klootzakken.Client/Activities/MainActivity.cs
@@ -13,33 +13,35 @@ using Klootzakken.Client.Utils;
 namespace Klootzakken.Client
 {
     [Activity(Label = "KlootzakkenClient", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : ActivityBase
     {
         private static bool _iocContainerInitialized;
-        public const string Page2Key = "TestActivity";
+
+        private const string _authenticationActivityPageKey = "AuthenticationActivity";
+        private const string _mainMenuActivityPageKey = "MainMenuActivity";
 
         protected override void OnCreate(Bundle bundle)
         {
-
             if (!_iocContainerInitialized)
             {
-                _iocContainerInitialized = true;
                 ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
                 var nav = new NavigationService();
-                nav.Configure(Page2Key, typeof(TestActivity));
+                nav.Configure(_authenticationActivityPageKey, typeof(AuthenticationActivity));
+                nav.Configure(_mainMenuActivityPageKey, typeof(MainActivity));
                 SimpleIoc.Default.Register<INavigationService>(() => nav);
 
                 var dialog = new DialogService();
                 SimpleIoc.Default.Register<IDialogService>(() => dialog);
+
+                var authenticationOptions = new AuthenticationOptions() { BaseUri = new Uri("http://www.glueware.nl/klootzakken/kz/") };
+                var authenticationService = new AuthenticationService(authenticationOptions);
+                var tempAuthTokenPoller = new TempAuthTokenPoller(authenticationService);
+                var authenticationController = new AuthenticationController(authenticationService, tempAuthTokenPoller, new SharedPreferenceHandler());
+                SimpleIoc.Default.Register<AuthenticationController>(() => authenticationController);
+
+                _iocContainerInitialized = true;
             }
-
-            var authenticationOptions = new AuthenticationOptions() { BaseUri = new Uri("http://www.glueware.nl/klootzakken/kz/") };
-            var authenticationService = new AuthenticationService(authenticationOptions);
-            var tempAuthTokenPoller = new TempAuthTokenPoller(authenticationService);
-            var authenticationController = new AuthenticationController(authenticationService, tempAuthTokenPoller, new SharedPreferenceHandler());
-            SimpleIoc.Default.Register<AuthenticationController>(() => authenticationController);
-
 
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
@@ -48,9 +50,9 @@ namespace Klootzakken.Client
 
             loginButton.Click += delegate
             {
-                StartActivity(typeof(AuthenticationActivity));
+                var navigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+                navigationService.NavigateTo(MainActivity._authenticationActivityPageKey);
             };
-            
         }
     }
 }
