94449cf1aa9f156eb215f3bc7014a2d6b11d1207
Daniel Moka <daniel.moka@hightechict.nl>
2017-03-28 19:31:08 +0200

    added the logic for pop window to the auth viewmodel

--- Klootzakken.Client/MVVM/ViewModel/AuthenticationViewModel.cs
+++ Klootzakken.Client/MVVM/ViewModel/AuthenticationViewModel.cs
@@ -1,26 +1,44 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using Klootzakken.Client.App.Authentication;
+using Microsoft.Practices.ServiceLocation;
+using System;
 
 namespace Klootzakken.Client.MVVM.ViewModel
 {
     public class PinGenerationViewModel : ViewModelBase
     {
         //relay = közvetit
-        private RelayCommand _incrementCommand;
+        private RelayCommand _popUpGeneratedPinAndActions;
 
-        public RelayCommand IncrementCommand
+        public RelayCommand PopUpGeneratedPinAndActions
         {
             get
             {
-                return _incrementCommand
-                    ?? (_incrementCommand = new RelayCommand(
-                    () =>
+                return _popUpGeneratedPinAndActions
+                    ?? (_popUpGeneratedPinAndActions = new RelayCommand(
+                    async () =>
                     {
+                        var dialogService = ServiceLocator.Current.GetInstance<IDialogService>();
+                        var authenticationController = ServiceLocator.Current.GetInstance<AuthenticationController>();
+                        var pinCode = await authenticationController.GetPinCodeAsync();
 
+                        await dialogService.ShowMessage(
+                           pinCode,
+                            "Pair the following pincode",
+                            "Log in",
+                            "Cancel",
+                            new Action<bool>((isConfirmed) =>
+                            {
+                                if (isConfirmed) {
+                                    authenticationController.SaveBearerAuthTokenAsync(pinCode);
+                                }
+                            }
+                            ));
                     }));
             }
         }
-
     }
 }
