7cfdd3196bd385fb0558dc000cc5c0397e901311
Daniel Moka <daniel.moka@hightechict.nl>
2017-03-28 19:31:26 +0200

    added usage o the ayth view model to the auth activity

--- Klootzakken.Client/Activities/AuthenticationActivity.cs
+++ Klootzakken.Client/Activities/AuthenticationActivity.cs
@@ -1,21 +1,39 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Klootzakken.Client.Activities;
 using GalaSoft.MvvmLight.Views;
 using Microsoft.Practices.ServiceLocation;
-using GalaSoft.MvvmLight.Ioc;
 using System;
 using Klootzakken.Client.App.Authentication;
-using Android.Content;
 using Klootzakken.Client.Utils;
+using Klootzakken.Client.MVVM.ViewModel;
+using GalaSoft.MvvmLight.Helpers;
 
 namespace Klootzakken.Client
 {
     [Activity(Label = "KlootzakkenClient", MainLauncher = false, Icon = "@drawable/icon")]
-    public class AuthorizationActivity : ActivityBase
+    public class AuthenticationActivity : ActivityBase
     {
         public const string maniMenuPageKey = "MainMenuActivity";
+
+        private PinGenerationViewModel _pinGenerationViewModel;
+        private Button _generatePinBtn;
+
+        public PinGenerationViewModel PinGenerationViewModel
+        {
+            get
+            {
+                return _pinGenerationViewModel ?? (_pinGenerationViewModel = new PinGenerationViewModel());
+            }
+        }
+
+        public Button GeneratePinButton
+        {
+            get
+            {
+                return _generatePinBtn ?? (_generatePinBtn = FindViewById<Button>(Resource.Id.generatePin));
+            }
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -25,22 +43,7 @@ namespace Klootzakken.Client
             var dialogService = ServiceLocator.Current.GetInstance<IDialogService>();
             var nav = ServiceLocator.Current.GetInstance<INavigationService>();
 
-            Button generatePinButton = FindViewById<Button>(Resource.Id.generatePin);
-
-            generatePinButton.Click += async delegate
-            {
-                var authenticationController = ServiceLocator.Current.GetInstance<AuthenticationController>();
-                var pinCode = await authenticationController.GetPinCodeAsync();
-
-                await dialogService.ShowMessage(
-                   pinCode,
-                    "Pair the following pincode",
-                    "Log in",
-                    "Cancel",
-                    new Action<bool>((isConfirmed) => authenticationController.SaveBearerAuthTokenAsync(pinCode)));
-                //TODO: add false
-                //TODO: add test for services seperately with the live server
-            };
+            GeneratePinButton.SetCommand("Click", PinGenerationViewModel.PopUpGeneratedPinAndActions);
 
             RunOnUiThread(() => Toast.MakeText(this, new SharedPreferenceHandler().GetPreference("bearer_token"), ToastLength.Long).Show());
         }
